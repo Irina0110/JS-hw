8c7005ed4a4dd8782b1b6d5acac88cf684ea7778
Irina Safonova <irinasafonova2001@mail.ru>
2022-04-07 18:27:55 +0300

    HW 5 исправления

--- hw5/hw5.js
+++ hw5/hw5.js
@@ -5,10 +5,23 @@
 function range(x, y, k=1){
     let arr = [];
     let elem;
-    while (y >= x) {
-        elem = x;
-        arr.push(elem)
-        x += k;
+    if ((y > x) && (k > 0)) {
+        while (y >= x) {
+            elem = x;
+            arr.push(elem)
+            x += k;
+        }
+    }
+    if ((k < 0) && (y < x)){
+        while (x >= y) {
+            elem = y;
+            arr.push(elem)
+            x += k;
+        }
+    }
+    if (((k < 0) && (y > x)) || ((k > 0) && (y < x))) {
+        console.warn()
+        return null;
     }
     return arr;
 }
@@ -18,24 +31,56 @@ console.log(range(3, 9)); //вернет массив вида [3, 4, 5, 6, 7, 8
 
 //Задача 2
 
+// function checkSpam (a, ...spamWords){
+//     let del = " ";
+//     let arrWords = a.split(del);
+//     let period = 0;
+//     for (let i = 0; i <= arrWords.length; i++){
+//         for (let j = 0; j <= spamWords.length; j++){
+//             if (arrWords[i] === spamWords[j]){
+//                 period += 1;
+//             }
+//         }
+//     }
+//         if (period === 0) {
+//             return "Спам не встречается";
+//         }
+//         if ((period/arrWords.length) <= .5) {
+//             return "Спам-слово встречается редко";
+//         }
+//             return  "Спам-слово встречается часто";
+//
+// }
+
+//Задача 2
+
 function checkSpam (a, ...spamWords){
     let del = " ";
     let arrWords = a.split(del);
-    let period = 0;
-    for (let i = 0; i <= arrWords.length; i++){
-        for (let j = 0; j <= spamWords.length; j++){
+    let period = [];
+    for (let i = 0; i < arrWords.length; i++){
+        for (let j = 0; j < spamWords.length; j++){
             if (arrWords[i] === spamWords[j]){
-                period += 1;
+                if (!period[j]) {
+                    period[j] = 1;
+                } else
+                period[j] += 1;
             }
         }
     }
-        if (period === 0) {
-            return "Спам не встречается";
+    console.log(period)
+    for (let i = 0; i <= period.length; i++){
+        if ((period[i] === 0) || (!period[i])) {
+            console.log(spamWords[i] + " Спам-слово не встречается");
         }
-        if ((period/arrWords.length) <= .5) {
-            return "Спам-слово встречается редко";
+        if (period[i] <= 4) {
+            console.log(spamWords[i] + " Спам-слово встречается редко");
         }
-            return  "Спам-слово встречается часто";
+        if (period[i] >= 5) {
+            console.log(spamWords[i] + " Спам-слово встречается часто");
+        }
+    }
+    return arrWords;
 
 }
-console.log(checkSpam("sjbgv dlrbr ball ball spam ball ebrb", "spam", "ball", "rush", ));
+console.log(checkSpam("sjbgv dlrbr ball ball ball ball spam ball ebrb", "spam", "ball", "rush"));
